2d142f0e2e6ba0b598529143b79109871dd06888
onisim_albarov <onisim93@gmail.com>
2022-11-27 10:47:18 +0400

    complete module4-conditions-task3

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -1,6 +1,17 @@
 package school.mjc.stage0.conditions.task3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        char upperCaseChar = Character.toUpperCase(character);
+        List<Character> vowelChars = List.of('A', 'E', 'I','O','U','Y');
+                ;
+        if (upperCaseChar >= 65 && upperCaseChar <= 90) {
+            if (vowelChars.contains(upperCaseChar)) System.out.println("Vowel");
+            else System.out.println("Consonant");
+        }
+        else System.out.println("wrong alphabet!");
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -1,6 +1,13 @@
 package school.mjc.stage0.conditions.task3;
 
+import java.util.List;
+
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
+        List<Integer> monthsWith31Days = List.of(1,3,5,7,8,10,12);
+        if (month > 12 || month < 1) System.out.println("wrong number!");
+        else if (month == 2) System.out.println(28);
+        else if (monthsWith31Days.contains(month)) System.out.println(31);
+        else System.out.println(30);
     }
 }
